8270a7d28ad333205c2d6b67e648b1f5ff1b08da
shueppin <77411207+shueppin@users.noreply.github.com>
2025-01-20 00:04:32 +0100

    Changes to the starting script including output to a log file

--- starting_file/start.cmd
+++ starting_file/start.cmd
@@ -12,13 +12,27 @@ set "start_file=$start_file$"
 set "arguments=$arguments$"
 set "source_code_url=$source_code_url$"
 set "program_name=$program_name$"
+set "log_file=%~dp0\log.txt"
+
+
+:: Set the time and date
+for /f "tokens=*" %%d in ('date /t') do (set actualDate=%%d)
+for /f "tokens=*" %%t in ('time /t') do (set actualTime=%%t)
 
 
 :: Debug mode
 if /I "%debug%"=="true" (
+    :: Show some info to the log file
+    echo. >> %log_file%
+    echo. >> %log_file%
+    echo The starting script for %program_name% was executed at %actualDate%%actualTime% in DEBUG mode >> %log_file%
+    echo. >> %log_file%
+
+    :: Show some info to the console
     echo DEBUG is activated
     echo.
     echo Source code for %program_name% script can be found at %source_code_url%
+    echo The log file can be found at %log_file%
     echo.
     echo Variables:
     echo show_console = "%show_console%"
@@ -28,7 +42,7 @@ if /I "%debug%"=="true" (
     echo Starting with console open
     echo.
 
-    start "%program_name%" "python\python.exe" "%start_file%" %arguments%
+    start "%program_name%" "%~dp0\python\python.exe" "%start_file%" %arguments% 2>> %log_file%
 
     pause
 
@@ -36,16 +50,24 @@ if /I "%debug%"=="true" (
 )
 
 
+:: Show some info to the log file
+echo. >> %log_file%
+echo. >> %log_file%
+echo The starting script for %program_name% was executed at %actualDate%%actualTime% >> %log_file%
+echo. >> %log_file%
+
+
 :: Start the script either with or without console
 if /I "%show_console%"=="true" (
-    start "%program_name%" "python\python.exe" "%start_file%" %arguments%
+    start "%program_name%" "%~dp0\python\python.exe" "%start_file%" %arguments% 2>> %log_file%
 ) else if /I "%show_console%"=="false" (
-    start "%program_name%" "python\pythonw.exe" "%start_file%" %arguments%
+    :: It needs to be started like this in the background so the python output can be written to the log file.
+    start /B "%program_name%" "%~dp0\python\pythonw.exe" %start_file% %arguments% >> %log_file% 2>&1
 ) else (
     :: If the variable is not set then output this to the console and just start the script with the console open
     echo Please set "show_console" either to "true" or to "false"
     timeout /t 5
-    start "%program_name%" "python\python.exe" "%start_file%" %arguments%
+    start "%program_name%" "%~dp0\python\python.exe" "%start_file%" %arguments% 2>> %log_file%
 )
 
 
@@ -54,6 +76,7 @@ if /I "%show_console%"=="true" (
 if errorlevel 1 (
     echo There was an error when trying to execute the given file.
     echo For support please go to %source_code_url%
+    echo The cmd file has crashed >> %log_file%
     pause
 )
 
